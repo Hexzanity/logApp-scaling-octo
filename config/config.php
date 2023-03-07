3d6190c18b21271986540bff909026c5c4720257
HectorA.HerreraJr <201140489@psu.palawan.edu.ph>
2023-03-07 12:24:52 +0800

    Under Construction

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define('ROOT_URL', 'index.php');
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', 'Loveisshit143');
-	define('DB_NAME', 'Hexzy');
-?>
--- config/db.php
+++ config/db.php
@@ -1,9 +1,5 @@
-<?php
-	// Create Connection
-	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+<?php 
 
-	// Check Connection
-	if(mysqli_connect_errno()){
-		// Connection Failed
-		echo 'Failed to connect to MySQL '. mysqli_connect_errno();
-	}
+$db = mysqli_connect("localhost","root","Loveisshit143","recordsapp");
+
+?>
